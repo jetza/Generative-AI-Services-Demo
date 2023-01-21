b32fdbe2fab12c098d4d522c2ca086f5c075a0f8
JelenaM <jelena.miodragovic@seavus.com>
2023-01-21 19:13:02 +0100

    initial commit with app configuration and basic example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { Configuration, OpenAIApi } from "openai";
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [image, setImage] = useState("");
+    const [prompt, setPrompt] = useState("")
+
+  const configuration = new Configuration({
+     organization: "org-iw4ja26Oph0uy3PIFUNr56hK",
+    apiKey: process.env.REACT_APP_API_KEY
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      // model: "text-davinchi-002",
+      // prompt: "This is a test",
+      // temperature: 0,
+      // max_tokens: 6,
+      prompt: `${prompt}`,
+      n: 2,
+      size: "1024x1024",
+    })
+    setImage(response.data.data[0].url);
+  }
+
+    return (
+        <div>
+            <input onChange={(e) => setPrompt(e.target.value)}
+                   placeholder="Enter description of an image"
+            />
+            <button onClick={generateImage}>
+                Generate an image
+            </button>
+            <div>
+                <img src={image} alt=""/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
